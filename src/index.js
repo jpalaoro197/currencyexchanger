e0bbdf316e0a1b462d7d13a4b99265070aa859d9
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-01 15:41:11 -0700

    WIP project still isnt functioning got rid of all terminal issues, continuing to debug.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyService from './currency-service.js'
+import CurrencyService from './currency-service.js';
 
 function getElements (response, usd, selectedCurrency ) {
   if (response.conversion_rates) {
     const exchange = (usd / response.conversion_rates.USD);
-    $(".rate").text(`${exchange}`);
+    $(".rate").text(`${exchange} ${selectedCurrency}`);
   } else {
-    $(".error").text(`error occured: ${response.error_type}`)
+    $(".error").text(`error occured: ${response.error_type}`);
   }
 }
 
@@ -21,10 +21,9 @@ async function callApi(selectedCurrency, usd) {
 $(document).ready(function() {
   $('#currency-exchange').submit(function() {
     event.preventDefault();
-    let selectedCurrency =
-    let usd =
-    .then(function(response) {
-      getConversion(response);
+    let selectedCurrency = $("#selected-currency").val();
+    let usd = parseFloat($("#user-currency").val());
+    callApi(selectedCurrency, usd);
+    
     });
   });
-});
