3249b84e53d11ece22ed54d68847d7a6b86e8bc3
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-01 15:14:32 -0700

    correct get elements function, and add apicall async function user logic is still WIP

--- src/index.js
+++ src/index.js
@@ -4,19 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyService from './currency-service.js'
 
-function getConversion (response, usd, ) {
+function getElements (response, usd, selectedCurrency ) {
   if (response.conversion_rates) {
-    $(".rate").text(`${response.conversion_rates}`);
+    const exchange = (usd / response.conversion_rates.USD);
+    $(".rate").text(`${exchange}`);
   } else {
     $(".error").text(`error occured: ${response.error_type}`)
   }
 }
 
+async function callApi(selectedCurrency, usd) {
+  const response = await CurrencyService.getConversion(selectedCurrency, usd);
+  getElements(response, usd, selectedCurrency);
+}
+
 $(document).ready(function() {
   $('#currency-exchange').submit(function() {
-    const currency = $('#user-currency').val
-    $('#user-currency').val
-    CurrencyService.getExchange();
+    event.preventDefault();
+    let selectedCurrency =
+    let usd =
     .then(function(response) {
       getConversion(response);
     });
