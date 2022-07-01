c170e136bd1e8e384db9a19c914f56096dbcc610
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-01 09:56:20 -0700

    add service logic

---
+++ src/currency-service.js
@@ -0,0 +1,14 @@
+export default class CurrencyService {
+ static getConversion() {
+  return fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  .then (function(response) {
+    if (!response.ok) {
+      throw Error(response.error-type);
+    }
+    return response.json();
+  })
+  .catch(function(error) {
+    return error;
+  })
+};
+};
